b1d7f0c934d898028a39d6724d420538aa8e5129
Lee Morgan <lee@leemorgan.dev>
2024-07-19 16:20:48 -0400

    Data from user is now saved to the database.

--- src/routes/user.rs
+++ src/routes/user.rs
@@ -1,21 +1,31 @@
-use crate::models;
+use crate::models::{user::User};
 use actix_web::{web, Responder};
 use chrono::Utc;
 use mongodb::Client;
+use serde::Deserialize;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::resource("/user")
-            .route(web::get().to(create))
+            .route(web::post().to(create))
     );
 }
 
-async fn create(client: web::Data<Client>) -> impl Responder {
-    let new_user = models::user::User {
-        email: String::from("lee@leemorgan.dev"),
-        password: String::from("password123"),
-        first_name: String::from("Lee"),
-        last_name: String::from("Morgan"),
+#[derive(Deserialize)]
+struct CreateBody {
+    email: String,
+    password: String,
+    confirm_password: String,
+    first_name: String,
+    last_name: String
+}
+
+async fn create(client: web::Data<Client>, body: web::Json<CreateBody>) -> impl Responder {
+    let new_user = User {
+        email: body.email.clone(),
+        password: body.password.clone(),
+        first_name: body.first_name.clone(),
+        last_name: body.last_name.clone(),
         status: String::from("active"),
         expiration: Utc::now(),
         created_date: Utc::now(),
